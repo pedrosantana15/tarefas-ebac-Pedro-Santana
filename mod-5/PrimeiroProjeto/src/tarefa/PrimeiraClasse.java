2c5d38a1e4d300e15921d8dcf73d0430738e49c0
pedrosantana15 <137354621+pedrosantana15@users.noreply.github.com>
2024-05-05 18:22:19 -0300

    Adicionando meu primeiro código Java ao repositório

---
+++ mod-5/PrimeiroProjeto/src/tarefa/PrimeiraClasse.java
@@ -0,0 +1,12 @@
+package tarefa;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		
+		String strName = args[0];
+		
+		System.out.print("Hello " + strName + "!");
+	}
+
+}
