cff9983ddb513914e07b659ed783e8e57c5c01bd
pedrosantana15 <137354621+pedrosantana15@users.noreply.github.com>
2024-05-06 21:27:36 -0300

    Adicionando tarefa de Wrappers

---
+++ mod-9/TarefaWrapper/src/br/com/pedro/Main.java
@@ -0,0 +1,38 @@
+package br.com.pedro;
+
+public class Main {
+
+    public static void main(String args[]){
+
+        //Exemplo 1
+        //Valor primitivo
+        int numP = 18;
+
+        //Convertendo para Wrapper
+        Integer numW = numP;
+
+        System.out.println(numW);
+
+        //Exemplo 2
+        //Valor primitivo
+        boolean statusP = true;
+
+        //Convertendo para Wrapper
+        Boolean statusW = statusP;
+
+        System.out.println(statusW);
+
+        //Exemplo 3
+        //Valor primitivo
+        char letraP = 'A';
+
+        //Convertendo para Wrapper
+        Character letraW = letraP;
+
+        System.out.println(letraW);
+
+
+
+    }
+
+}
