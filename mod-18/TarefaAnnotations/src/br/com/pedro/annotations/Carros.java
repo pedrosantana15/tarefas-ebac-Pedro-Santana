4016656eb95bac936e1a362981af90d9234bde30
pedrosantana15 <137354621+pedrosantana15@users.noreply.github.com>
2024-05-14 14:07:46 -0300

    Adicionando tarefa de Annotations

---
+++ mod-18/TarefaAnnotations/src/br/com/pedro/annotations/Carros.java
@@ -0,0 +1,21 @@
+package br.com.pedro.annotations;
+
+
+public class Carros {
+
+    @Tabela(value = "Tabela Ford")
+    public void CarrosFord(){
+        //Tabela    
+    }
+
+    @Tabela(value = "Tabela Fiat")
+    public void CarrosFiat(){
+        //Tabela
+    }
+
+    @Tabela(value = "Tabela Honda")
+    public void CarrosHonda(){
+        //Tabela
+    }
+
+}
