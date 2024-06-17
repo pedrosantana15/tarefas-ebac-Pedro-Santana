33367d68791697b52730faf1f394c4f7d7a4deea
pedrosantana15 <137354621+pedrosantana15@users.noreply.github.com>
2024-06-17 13:29:25 -0300

    Adicionando depêndencias com Maven

---
+++ mod-37/PrimeiroProjetoMaven/PrimeiroProjetoMaven/src/test/java/br/com/spedro/AppTest.java
@@ -0,0 +1,16 @@
+package br.com.spedro;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void mainTest(){
+
+    }
+
+}
